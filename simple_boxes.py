268b830108a26bde9f554cf079873e0eaccdc11f
Pio Calderon <piocalderon@gmail.com>
2014-07-07 21:59:57 +0800

    Update simple_boxes.py
    cleaned up

--- simple_boxes.py
+++ simple_boxes.py
@@ -10,63 +10,62 @@ from PIL import Image
 from imageprocessing import plot_cosine_similarity
 import matplotlib.pyplot as plt
 
-def constructChessGrid(gridSize, sizeBox, probability, run, mutation): #probability of being black
+def constructChessGrid(gridSize, sizeBox, probability, run, mutation):
+    """
+    Construct a chess grid with various input features.
+    """
+
     grid = np.array([[[255,255,255,255]]*gridSize]*gridSize)
-#    rowIndex1, rowIndex2 = gridSize/2 - sizeBox/2, gridSize/2 +sizeBox/2
-#    colIndex1, colIndex2 = gridSize/2 - sizeBox/2, gridSize/2 +sizeBox/2
-#    grid[rowIndex1:rowIndex2, colIndex1: colIndex2] = [[[0,0,0,255]]*sizeBox]*sizeBox
-#    
-    numBox = gridSize/sizeBox    
+    numBox = gridSize/sizeBox   
+    
     for i in xrange(numBox):
         for j in xrange(numBox):
             rowIndex1, rowIndex2 = i*sizeBox, (i+1)*sizeBox
             colIndex1, colIndex2 = j*sizeBox, (j+1)*sizeBox
             if np.random.uniform() < probability:
-                #grid[rowIndex1:rowIndex2, colIndex1: colIndex2] = [[(0,0,0)]*sizeBox]*sizeBox
                 grid[rowIndex1:rowIndex2, colIndex1: colIndex2] = [[[0,0,0,255]]*sizeBox]*sizeBox
-#    for i in xrange(len(grid)):
-#        for j in xrange(len(grid[0])):
-#            if np.random.uniform() < mutation:
-#                if list(grid[i][j]) == [255,255,255,255]:
-#                    grid[i][j] = [0,0,0,255]
-#                else:
-#                    grid[i][j] = [255,255,255,255]
     im = Image.fromarray(np.uint8(grid), mode="RGBA")
     im.save("grid_s{0}_sb{1}_p{2}_run{3}_m{4}.png".format(gridSize,sizeBox, probability, run,mutation))
     return grid
-    
-sizeBoxList = [64,32,16,8,4,2,1]#[1,2,4,8,16,32,64]
+
+def main(sizeBoxList, probabilities, mutations, numRun, size)
+    for sizeBox in sizeBoxList:
+        for mutation in mutations:
+            for probability in probabilities:
+                g=open('RUNAVERAGED_size{0}sizebox{1}mutation{2}probability{3}.csv'.format(size,sizeBox, mutation, probability), 'w')
+                g.write('framelength,similarity,std\n')
+                runList = []
+                for run in xrange(numRun):
+                    print "start: sizeBox: ", sizeBox, " ;p: ", probability, "run: ", run
+                    grid = constructChessGrid(size, sizeBox, probability, run, mutation)      
+                    ratio,similarityList = plot_cosine_similarity(grid,r'grid_s{0}_sb{1}_p{2}_run{3}_m{4}.png'.format(size,sizeBox, probability, run, mutation))
+                    runList.append(similarityList) 
+                runList = zip(*runList)
+                runMean = np.mean(runList,1)
+                runErr = np.std(runList,1)/np.sqrt(numRun)
+                    #        plt.plot(ratio, similarityList,label="p={0}".format(probability))
+                plt.errorbar(ratio, runMean, yerr=runErr,label="p={0}".format(probability))
+                for index in xrange(len(ratio)):
+                    g.write("{0},{1},{2}\n".format(ratio[index],runMean[index],runErr[index]))
+                g.close()
+            plt.xlabel(r'relative frame length $k$')
+            plt.ylabel(r'cos $\theta$')
+            plt.ylim(ymin = 0.5, ymax=1)
+            plt.legend(loc="lower right")
+            plt.savefig("CS_grid_s{0}_sb{1}_m{2}.png".format(size,sizeBox,mutation),bbox_inches="tight")
+            plt.close()
+        
+sizeBoxList = [64,32,16,8,4,2,1]
 probabilities = [0.25,0.375,0.5]
 mutations = [0]
 numRun=10
 size = 128
 
-
-for sizeBox in sizeBoxList:
-    for mutation in mutations:
-        for probability in probabilities:
-            g=open('RUNAVERAGED_size{0}sizebox{1}mutation{2}probability{3}.csv'.format(size,sizeBox, mutation, probability), 'w')
-            g.write('framelength,similarity,std\n')
-            runList = []
-            for run in xrange(numRun):
-                print "start: sizeBox: ", sizeBox, " ;p: ", probability, "run: ", run
-                grid = constructChessGrid(size, sizeBox, probability, run, mutation)      
-                ratio,similarityList = plot_cosine_similarity(grid,r'grid_s{0}_sb{1}_p{2}_run{3}_m{4}.png'.format(size,sizeBox, probability, run, mutation))
-                runList.append(similarityList) 
-            runList = zip(*runList)
-            runMean = np.mean(runList,1)
-            runErr = np.std(runList,1)/np.sqrt(numRun)
-                #        plt.plot(ratio, similarityList,label="p={0}".format(probability))
-            plt.errorbar(ratio, runMean, yerr=runErr,label="p={0}".format(probability))
-            for index in xrange(len(ratio)):
-                g.write("{0},{1},{2}\n".format(ratio[index],runMean[index],runErr[index]))
-            g.close()
-        plt.xlabel(r'relative frame length $k$')
-        plt.ylabel(r'cos $\theta$')
-        plt.ylim(ymin = 0.5, ymax=1)
-        plt.legend(loc="lower right")
-        plt.savefig("CS_grid_s{0}_sb{1}_m{2}.png".format(size,sizeBox,mutation),bbox_inches="tight")
-        plt.close()
-        
-
-        
+if __name__ == "__main__":
+    sizeBoxList = [64,32,16,8,4,2,1]
+    probabilities = [0.25,0.375,0.5]
+    mutations = [0]
+    numRun=10
+    size = 128
+    
+    main(sizeBoxList, probabilities, mutations, numRun, size)
